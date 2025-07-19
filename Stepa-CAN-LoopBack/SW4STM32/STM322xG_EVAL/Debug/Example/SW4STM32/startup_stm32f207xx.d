fe08afab930abdd667cd8c77cea9eb1025c38a76
Stepan <142824796+Aurasnim@users.noreply.github.com>
2025-07-19 21:34:00 +0300

    Delete Stepa-CAN-LoopBack directory

--- Stepa-CAN-LoopBack/SW4STM32/STM322xG_EVAL/Debug/Example/SW4STM32/startup_stm32f207xx.d
+++
@@ -1,2 +0,0 @@
-Example/SW4STM32/startup_stm32f207xx.o: \
- C:/Users/123/STM32CubeIDE/Repository/STM32Cube_FW_F2_V1.9.5/Projects/STM322xG_EVAL/Examples/CAN/CAN_LoopBack/SW4STM32/startup_stm32f207xx.s
